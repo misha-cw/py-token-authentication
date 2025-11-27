04495892d7a4ff5108876eb449d3f4fc920f3922
misha-cw <bigun.mykhaylo@gmail.com>
2025-11-27 13:57:28 +0200

    Implement user authentication and permissions
    - Added IsAdminOrIfAuthenticatedReadOnly permission class.
    - Updated viewsets to use the new permission class.
    - Configured REST framework to use TokenAuthentication.
    - Created user registration, login, and management endpoints.
    - Added UserSerializer for user data handling.

--- user/urls.py
+++ user/urls.py
@@ -1,1 +1,16 @@
-# write your code here
+from django.urls import path
+
+from user.views import (
+    ManageUserView,
+    UserCreateView,
+    CreateTokenView,
+)
+
+
+app_name = "user"
+
+urlpatterns = [
+    path("register/", UserCreateView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+]
